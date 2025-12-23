dee3b2c0161c427079af17203ca4dc02a0dd3722
manas24103 <manasgupta24103@gmail.com>
2025-12-23 20:06:44 +0530

    configured data base connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  try{
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
+    console.error(`Error connecting to MongoDB:`, error);
     process.exit(1);
   }
-};
+}
 
 export default connectDB;

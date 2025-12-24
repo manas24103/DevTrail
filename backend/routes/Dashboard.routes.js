0287d4faa4d4452c25a1f101686e382e488f9125
manas24103 <manasgupta24103@gmail.com>
2025-12-24 15:47:00 +0530

    trying to make it work

--- backend/routes/Dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getDashboard } from '../controllers/dashboard.Controller.js';
-import { verifyJWT } from '../middleware/auth.middleware.js';
-
-const router = Router();
-
-router.route('/').get(verifyJWT, getDashboard);
-
-export default router;
